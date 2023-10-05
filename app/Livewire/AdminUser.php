ce026309d747e49ecc578c11da2db23d63f43c9d
Rubén Chanamé <rdchaname@gmail.com>
2023-10-04 22:47:36 -0500

    Buscar usuarios

--- app/Livewire/AdminUser.php
+++ app/Livewire/AdminUser.php
@@ -3,23 +3,34 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Database\Query\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
+
+use function PHPUnit\Framework\isEmpty;
 
 class AdminUser extends Component
 {
     use WithPagination;
 
-    public string $email;
-    public string $name;
+    public string $email = '';
+    public string $name = '';
 
     public function render()
     {
-        $users = User::paginate(1);
+        $users = User::where(function ($query) {
+            if (!empty($this->name)) {
+                $query->where('name', 'LIKE', '%' . $this->name . '%');
+            }
+            if (!empty($this->email)) {
+                $query->where('email', 'LIKE', '%' . $this->email . '%');
+            }
+        })->paginate();
         return view('livewire.admin-user', compact('users'));
     }
 
     public function buscar()
     {
+        $this->resetPage();
     }
 }
--- resources/views/livewire/admin-user.blade.php
+++ resources/views/livewire/admin-user.blade.php
@@ -1,32 +1,30 @@
 <div>
     <div
         class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
-        <form wire:submit.prevent='buscar'>
+        <form wire:submit.prevent='buscar' autocomplete="off">
             <div class="border-gray-900/10 pb-2">
                 <div class="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
                     <div class="sm:col-span-2 sm:col-start-1">
-                        <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Correo
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Correo
                             electrónico</label>
                         <div class="mt-2">
-                            <input wire:model="email" type="text" name="city" id="city"
-                                autocomplete="address-level2"
+                            <input wire:model="email" type="text" name="email" id="email"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
                         <div class="mt-2">
-                            <input wire:model="name" type="text" name="region" id="region"
-                                autocomplete="address-level1"
+                            <input wire:model="name" type="text" name="name" id="name"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="region" class="block text-sm font-medium leading-6 text-gray-900">&nbsp;</label>
+                        <label class="block text-sm font-medium leading-6 text-gray-900">&nbsp;</label>
                         <div class="mt-2">
-                            <button type="submit"
-                                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Buscar</button>
+                            <x-button type="submit" id="btnBuscar">Buscar</x-button>
+                            <x-success-button id="btnNuevo">Registrar nuevo</x-success-button>
                         </div>
                     </div>
                 </div>
@@ -36,27 +34,35 @@
 
     <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8 p-6 lg:p-8">
         <div>
-            <div class="flex flex-col overflow-x-auto">
+            <div class="flex flex-col">
                 <div class="sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                        <div class="overflow-x-auto mb-4">
+                        <div class="mb-4">
                             <table class="min-w-full text-left text-sm font-light">
                                 <thead class="border-b font-medium dark:border-neutral-500">
                                     <tr>
-                                        <th scope="col" class="px-6 py-4">#</th>
-                                        <th scope="col" class="px-6 py-4">Nombre</th>
-                                        <th scope="col" class="px-6 py-4">Email</th>
-                                        <th colspan="2" scope="col" class="px-6 py-4">Email</th>
+                                        <th scope="col" class="px-6 py-4 text-center">#</th>
+                                        <th scope="col" class="px-6 py-4 text-center">Nombre</th>
+                                        <th scope="col" class="px-6 py-4 text-center">Email</th>
+                                        <th colspan="2" scope="col" class="px-6 py-4 text-center">Operaciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach ($users as $user)
                                         <tr class="border-b dark:border-neutral-500">
-                                            <td class="whitespace-nowrap px-6 py-4">{{ $user->ud }}</td>
+                                            <td class="whitespace-nowrap px-6 py-4 text-center">{{ $user->id }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $user->name }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $user->email }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4">Editar</td>
-                                            <td class="whitespace-nowrap px-6 py-4">Eliminar</td>
+                                            <td class="whitespace-nowrap py-4">
+                                                <x-warning-button>
+                                                    Editar
+                                                </x-warning-button>
+                                            </td>
+                                            <td class="whitespace-nowrap py-4">
+                                                <x-danger-button>
+                                                    Eliminar
+                                                </x-danger-button>
+                                            </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
